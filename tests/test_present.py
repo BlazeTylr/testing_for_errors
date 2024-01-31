46984a17958bfb1c7ff6bad16a1a089e932bd8e4
balazs szabo <csikai.b@gmail.com>
2024-01-31 17:25:30 +0000

    tests complete

--- tests/test_present.py
+++ tests/test_present.py
@@ -1,4 +1,17 @@
+import pytest 
 from lib.present import *
 
-def testing_for_errors():
-  pass
+def testing_wrap_function():
+    present = Present()
+    present.wrap('Present')
+    with pytest.raises(Exception) as err:
+        present.wrap('Gift')
+    error_message = str(err.value)
+    assert error_message == "A contents has already been wrapped."
+
+def testing_unwrap_function():
+    present = Present()
+    with pytest.raises(Exception) as err:
+        present.unwrap()
+    error_message = str(err.value)
+    assert error_message == "No contents have been wrapped."
