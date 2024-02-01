e102c385adee0d7799222ebf4fec554f6af26a65
balazs szabo <csikai.b@gmail.com>
2024-02-01 11:58:58 +0000

    added password checker, test complete

--- lib/present.py
+++ lib/present.py
@@ -11,3 +11,10 @@ class Present:
         if self.contents is None:
             raise Exception("No contents have been wrapped.")
         return self.contents
+
+class PasswordChecker:
+    def check(self, password):
+        if len(password) >= 8:
+            return True
+        else:
+            raise Exception("Invalid password, must be 8+ characters.")
--- tests/test_present.py
+++ tests/test_present.py
@@ -15,3 +15,17 @@ def testing_unwrap_function():
         present.unwrap()
     error_message = str(err.value)
     assert error_message == "No contents have been wrapped."
+
+def test_password_checker_checking_true_value():
+    new_password = PasswordChecker()
+    true_pass = new_password.check('12345678')
+    true_return_value = True
+    assert true_pass == true_return_value
+
+
+def test_password_checker_checking_error_handling():
+    new_password = PasswordChecker()
+    with pytest.raises(Exception) as e:
+        new_password.check('1234')
+    error_message = str(e.value)
+    assert error_message == 'Invalid password, must be 8+ characters.'
